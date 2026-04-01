22195a1fe9e908c5cf82f329c29f9944bd907b5a
judehoy <blackbeltjude@gmail.com>
2026-04-01 17:27:19 -0400

    styling

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ document.addEventListener("DOMContentLoaded", function () {
         startButton.addEventListener("click", closePopup);
     }
 });
+function clickedMe(){
+    let objectClicked = document.getElementById('clickable');
+    objectClicked.classList.add('purple');
+}
+function pageLocation(page){
+    window.location.href = page + ".html";
+}
--- srcipt.js
+++
@@ -1,7 +0,0 @@
-function clickedMe(){
-    let objectClicked = document.getElementById('clickable');
-    objectClicked.classList.add('purple');
-}
-function pageLocation(page){
-    window.location.href = page + ".html";
-}
