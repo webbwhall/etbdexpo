ec4fe55bb787e20d56719d7e2d8717089f36cc1e
judehoy <blackbeltjude@gmail.com>
2026-04-01 16:50:04 -0400

    updated style

--- srcipt.js
+++ srcipt.js
@@ -2,3 +2,6 @@ function clickedMe(){
     let objectClicked = document.getElementById('clickable');
     objectClicked.classList.add('purple');
 }
+function pageLocation(page){
+    window.location.href = page + ".html";
+}
